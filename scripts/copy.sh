edc788b639d7f495cdf6743b66bad365ac742840
MinaSameh1 <minasameh1@gmail.com>
2021-12-18 23:23:22 +0200

    Modified Script to support args

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+set -x
+
 XPDIR="/home/mina/Documents/stuff/college/y4/asp.net/project/mount/Documents?and?Settings/admin/Desktop/project/"
 
 baseDir="/home/mina/Documents/stuff/college/y4/asp.net/project/."
 
-rsync --protect-args -aruzv $XPDIR $baseDir
+Mount="mount/"
+
+[ $1 -eq 1 ] && rsync --protect-args -aruzv $XPDIR $baseDir
+[ $1 -eq 2 ] && rsync --exclude $Mount --protect-args -aruzv $baseDir $XPDIR 
