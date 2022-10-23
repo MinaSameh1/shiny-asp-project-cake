410f1b1612bd442a8de5707034bc33b3d778e76a
MinaSameh1 <minasameh1@gmail.com>
2022-10-23 22:08:13 +0200

    Delete SQL/oldSQL directory

--- SQL/oldSQL/PDF.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `pdf` (
-	`pdfID` INT NOT NULL AUTO_INCREMENT PRIMARY key,
-	`bookID` INT NOT NULL DEFAULT 0,
-	`pdfLocation` TEXT(500) NOT NULL DEFAULT '',
-	CONSTRAINT `fk_pdfbookID` FOREIGN KEY (`bookID`) REFERENCES `books` (`BookID`)
-)
-COLLATE='utf8mb4_general_ci'
-;
